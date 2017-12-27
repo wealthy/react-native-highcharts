cd082b20163a76d1e703d5c29b00de336752c76c
harkirat <saluja.harkirat@gmail.com>
2017-12-27 17:15:14 +0530

    fixing android tab not changing bug

--- App/index.js
+++ App/index.js
@@ -34,98 +34,6 @@ const generateHTML = (config) => (
     <div id="container"></div>
       <script>
            ;(function () {
-             
-              var promiseChain = Promise.resolve();
-          
-          
-              var promises = {};
-              var callbacks = {};
-          
-             var init = function() {
-          
-                 const guid = function() {
-                     function s4() {
-                         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-                     }
-                     return s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4();
-                 }
-          
-                 window.webViewBridge = {
-                     /**
-                      * send message to the React-Native WebView onMessage handler
-                      * @param targetFunc - name of the function to invoke on the React-Native side
-                      * @param data - data to pass
-                      * @param success - success callback
-                      * @param error - error callback
-                      */
-                     send: function(targetFunc, data, success, error) {
-                        alert("in here");
-                         success = success || function(){};
-                         error = error || function () {};
-          
-                         var msgObj = {
-                             targetFunc: targetFunc,
-                             data: data || {},
-                             msgId: guid(),
-                         };
-          
-                         var msg = JSON.stringify(msgObj);
-          
-                         promiseChain = promiseChain.then(function () {
-                             return new Promise(function (resolve, reject) {
-                                 alert("in here too");
-                                 console.log("sending message " + msgObj.targetFunc);
-          
-                                 promises[msgObj.msgId] = {resolve: resolve, reject: reject};
-                                 callbacks[msgObj.msgId] = {
-                                     onsuccess: success,
-                                     onerror: error
-                                 };
-          
-                                 window.postMessage(msg, "*");
-                             })
-                         }).catch(function (e) {
-                             alert(e.message.toString());
-                             console.error('rnBridge send failed ' + e.message);
-                         });
-                     },
-          
-          
-                 };
-          
-                 window.document.addEventListener('message', function(e) {
-                     console.log("message received from react native");
-          
-                     var message;
-                     try {
-                         message = JSON.parse(e.data)
-                     }
-                     catch(err) {
-                         console.error("failed to parse message from react-native " + err);
-                         return;
-                     }
-          
-                     //resolve promise - send next message if available
-                     if (promises[message.msgId]) {
-                         promises[message.msgId].resolve();
-                         delete promises[message.msgId];
-                     }
-          
-                     //trigger callback
-                     if (message.args && callbacks[message.msgId]) {
-                         if (message.isSuccessfull) {
-                             callbacks[message.msgId].onsuccess.apply(message.args);
-                         }
-                         else {
-                             callbacks[message.msgId].onerror.apply(null, message.args);
-                         }
-                         delete callbacks[message.msgId];
-                     }
-          
-                 });
-             };
-          
-             init();
               Highcharts.chart('container',${config});
            }());
           </script>
@@ -133,6 +41,8 @@ const generateHTML = (config) => (
   </html>
   `
 );
+
+
 
 const win = Dimensions.get('window');
 class ChartWeb extends Component {
@@ -143,22 +53,23 @@ class ChartWeb extends Component {
     });
 
 
-
     config = JSON.parse(config);
-    const html = generateHTML(flattenObject(config));
+
+    let concatHTML = generateHTML(flattenObject(config));
 
     return (
         <View style={this.props.style}>
-            <WebView
-                style={styles.full}
-                source={{ html}}
-                javaScriptEnabled={true}
-                domStorageEnabled={true}
-                scalesPageToFit={true}
-                scrollEnabled={false}
-                automaticallyAdjustContentInsets={true}
-                {...this.props}
-            />
+          <WebView
+              onLayout={this.reRenderWebView}
+              style={styles.full}
+              source={{ html: concatHTML, baseUrl: 'http://test.com', }}
+              javaScriptEnabled={true}
+              domStorageEnabled={true}
+              scalesPageToFit={true}
+              scrollEnabled={false}
+              automaticallyAdjustContentInsets={true}
+              {...this.props}
+          />
         </View>
     );
   };
