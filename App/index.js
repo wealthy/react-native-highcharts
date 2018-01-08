5931107ef1e85233842b2d068bb59eef227d065f
Harkirat Saluja <saluja.harkirat@gmail.com>
2018-01-08 16:12:20 +0530

    Update index.js

--- App/index.js
+++ App/index.js
@@ -33,6 +33,99 @@ const generateHTML = (config) => (
     <body>
     <div id="container"></div>
       <script>
+            (function(){
+      
+          var promiseChain = Promise.resolve();
+      
+      
+          var promises = {};
+          var callbacks = {};
+      
+         var init = function() {
+      
+             const guid = function() {
+                 function s4() {
+                     return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+                 }
+                 return s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4() + s4();
+             }
+      
+             window.webViewBridge = {
+                 /**
+                  * send message to the React-Native WebView onMessage handler
+                  * @param targetFunc - name of the function to invoke on the React-Native side
+                  * @param data - data to pass
+                  * @param success - success callback
+                  * @param error - error callback
+                  */
+                 send: function(targetFunc, data, success, error) {
+                     success = success || function(){};
+                     error = error || function () {};
+      
+                     var msgObj = {
+                         targetFunc: targetFunc,
+                         data: data || {},
+                         msgId: guid(),
+                     };
+      
+                     var msg = JSON.stringify(msgObj);
+      
+                     promiseChain = promiseChain.then(function () {
+                         return new Promise(function (resolve, reject) {
+                             console.log("sending message " + msgObj.targetFunc);
+      
+                             promises[msgObj.msgId] = {resolve: resolve, reject: reject};
+                             callbacks[msgObj.msgId] = {
+                                 onsuccess: success,
+                                 onerror: error
+                             };
+      
+                             window.postMessage(msg);
+                         })
+                     }).catch(function (e) {
+                         console.error('rnBridge send failed ' + e.message);
+                     });
+                 },
+      
+      
+             };
+      
+             window.document.addEventListener('message', function(e) {
+                 console.log("message received from react native");
+      
+                 var message;
+                 try {
+                     message = JSON.parse(e.data)
+                 }
+                 catch(err) {
+                     console.error("failed to parse message from react-native " + err);
+                     return;
+                 }
+      
+                 //resolve promise - send next message if available
+                 if (promises[message.msgId]) {
+                     promises[message.msgId].resolve();
+                     delete promises[message.msgId];
+                 }
+      
+                 //trigger callback
+                 if (message.args && callbacks[message.msgId]) {
+                     if (message.isSuccessfull) {
+                         callbacks[message.msgId].onsuccess.apply(null, message.args);
+                     }
+                     else {
+                         callbacks[message.msgId].onerror.apply(null, message.args);
+                     }
+                     delete callbacks[message.msgId];
+                 }
+      
+             });
+         };
+      
+         init();
+      }());
+      </script>
+      <script>
            ;(function () {
               Highcharts.chart('container',${config});
            }());
@@ -47,6 +140,33 @@ const generateHTML = (config) => (
 const win = Dimensions.get('window');
 class ChartWeb extends Component {
 
+  onWebViewMessage = (event) => {
+    // post back reply as soon as possible to enable sending the next message
+    this.myWebView.postMessage(event.nativeEvent.data);
+    // alert(event.nativeEvent.data);
+    let msgData;
+    try {
+      msgData = JSON.parse(event.nativeEvent.data);
+    }    catch (err) {
+      console.warn(err);
+      return;
+    }
+
+    const value = msgData.data.mydata;
+    this.props.updateStore(value);
+
+    // invoke target function
+    const response = this[msgData.targetFunc].apply(this, [msgData]);
+    // trigger success callback
+    msgData.isSuccessfull = true;
+    msgData.args = [response];
+    this.myWebView.postMessage(JSON.stringify(msgData));
+  };
+
+  testFunc = () => {
+    return null;
+  }
+
   render() {
     let config = JSON.stringify(this.props.config, function (key, value) {//create string of json but if it detects function it uses toString()
       return (typeof value === 'function') ? value.toString() : value;
@@ -60,6 +180,9 @@ class ChartWeb extends Component {
     return (
         <View style={this.props.style}>
           <WebView
+              ref={(webview) => {
+                this.myWebView = webview;
+              }}
               onLayout={this.reRenderWebView}
               style={styles.full}
               source={{ html: concatHTML, baseUrl: 'http://test.com', }}
@@ -67,6 +190,7 @@ class ChartWeb extends Component {
               domStorageEnabled={true}
               scalesPageToFit={true}
               scrollEnabled={false}
+              onMessage={this.onWebViewMessage}
               automaticallyAdjustContentInsets={true}
               {...this.props}
           />
